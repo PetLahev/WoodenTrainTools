0c063b83147d3e7ff18a4fe83145a1fd03625ebe
PetLahev <premysl.lazecky@gmail.com>
2017-03-15 22:44:48 +0100

    Minor changes to the Rfid funcs. I'd like to somehow write and read from tag

--- Rifid_funcs.ino
+++ Rifid_funcs.ino
@@ -1,11 +1,13 @@
 
 // Increase the first dimension of the trains arrays when a new train is added
-int trains[1][4] = {
-	{ 0x52, 0x3D, 0x5A, 0xFF } // train 1
+int trains[2][4] = {
+	{ 0x2B, 0x45, 0xE7, 0xB9 }, // train 1 
+	{ 0x2A, 0x48, 0xA9, 0xAB }  // train 2
 };
 
 /*
-
+Responsible for managing the RFID reader
+If a tag is recognized, sends a message LED matrix
 */
 void rfidManager()
 {
@@ -17,14 +19,18 @@ void rfidManager()
 
 	if (isRecognized())
 	{
-		//TODO: Print the MIFARE_Key
+		//TODO: Print the MIFARE_Key		
 		Serial.println("train found");
+	}
+	else
+	{
+		Serial.print("The card is not recognized:");
+		writeCardTag(rfid.uid.uidByte, rfid.uid.size);
 	}
 
 	// finish the communication and crypting
 	rfid.PICC_HaltA();
 	rfid.PCD_StopCrypto1();
-
 }
 
 /*
@@ -32,20 +38,29 @@ Checks if a train is found in the array of trains
 Returns true if train is found, otherwise false
 */
 bool isRecognized()
-{
-	// TODO: Can I use sizeOf instead of hardcoded values?
-	// int size = sizeof(trains[0]) / sizeof(int);
+{	
+	int numOfTrains =	sizeof(trains) / sizeof(trains[0]);
 	bool retVal = true;
-	for (int i = 0; i < 1; i++)
+	for (int i = 0; i < numOfTrains; i++)
 	{
 		retVal = true;
-		for (int j = 0; j < 4; i++)
-		{
-			if (rfid.uid.uidByte[j] != trains[i][j]) {
+		for (int j = 0; j < 4; j++)
+		{			
+			if (rfid.uid.uidByte[j] != trains[i][j]) {				
 				retVal = false;
 				break;
 			}
 		}
 	}	
 	return retVal;
+}
+
+/*
+Writes card rags into the serial monitor
+*/
+void writeCardTag(byte *buffer, byte bufferSize) {
+	for (byte i = 0; i < bufferSize; i++) {
+		Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+		Serial.print(buffer[i], HEX);
+	}
 }
