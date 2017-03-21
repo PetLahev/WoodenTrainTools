37c0c43a2e70bb79adc65bac948c4764b0f4a86b
PetLahev <premysl.lazecky@gmail.com>
2017-03-21 22:02:01 +0100

    Finished the code for Rfid reader - fixed a few bugs

--- Rifid_funcs.ino
+++ Rifid_funcs.ino
@@ -1,9 +1,12 @@
 
 // Increase the first dimension of the trains arrays when a new train is added
 int trains[2][4] = {
-	{ 0x2B, 0x45, 0xE7, 0xB9 }, // train 1 
+	{ 0x00, 0x01, 0xB3, 0x4F }, // train 1 
 	{ 0x2A, 0x48, 0xA9, 0xAB }  // train 2
 };
+
+// Textual representation of tags TODO: Ideally this will be somehow written on the card
+String trainsDescription[] = { "Train 1", "Train 2" };
 
 /*
 Responsible for managing the RFID reader
@@ -17,10 +20,10 @@ void rfidManager()
 	// check if we can read the tag
 	if (!rfid.PICC_ReadCardSerial())return;
 
-	if (isRecognized())
-	{
-		//TODO: Print the MIFARE_Key		
-		Serial.println("train found");
+	int trainIndex = 0;
+	if (isRecognized(trainIndex))
+	{		
+		Serial.println(trainsDescription[trainIndex]);
 	}
 	else
 	{
@@ -36,23 +39,45 @@ void rfidManager()
 /*
 Checks if a train is found in the array of trains
 Returns true if train is found, otherwise false
+trainIndex - returns index of the train in the trains array
 */
-bool isRecognized()
+bool isRecognized(int &trainIndex)
 {	
-	int numOfTrains =	sizeof(trains) / sizeof(trains[0]);
+	int numOfTrains = sizeof(trains) / sizeof(trains[0]);	
 	bool retVal = true;
 	for (int i = 0; i < numOfTrains; i++)
 	{
 		retVal = true;
 		for (int j = 0; j < 4; j++)
 		{			
+			//printTags(i, j);
 			if (rfid.uid.uidByte[j] != trains[i][j]) {				
 				retVal = false;
 				break;
 			}
 		}
+		trainIndex = i;
+		if (retVal) break; // found the train, no need to do another loop
 	}	
 	return retVal;
+}
+
+/* Helper function*/
+void printTags(int trainIndex, int j)
+{
+	Serial.print("Tag[");
+	Serial.print(j);
+	Serial.print("] = ");
+	Serial.print(rfid.uid.uidByte[j]);
+	Serial.print(" == ");
+	Serial.print("Trains[");
+	Serial.print(trainIndex);
+	Serial.print("][");
+	Serial.print(j);
+	Serial.print("] = ");
+	Serial.print(trains[trainIndex][j]);
+	Serial.print(" => ");
+	Serial.println(rfid.uid.uidByte[j] == trains[trainIndex][j]);
 }
 
 /*
--- __vm/.WoodenTrainTools.vsarduino.h
+++ __vm/.WoodenTrainTools.vsarduino.h
@@ -69,5 +69,4 @@ extern "C" void __cxa_pure_virtual() {;}
 #define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
 #include "WoodenTrainTools.ino"
 #include "Rifid_funcs.ino"
-#include "T11_RFID_Tutorial_Sketch.ino"
 #endif
